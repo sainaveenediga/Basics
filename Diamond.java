4f4fbaa774befa27d212fbb7864dc144f31ba950
AlleKusuma <allekusuma059@gmail.com>
2024-08-01 11:51:29 +0530

    anna

--- Diamond.java
+++ Diamond.java
@@ -2,11 +2,12 @@ public class Diamond
 {
 	public static void main(String[] args)
 	{
+		System.out.println("helllo");
 		for(int i=1;i<=5;i++)
 		{
 			for(int j=5;j>=1;j--)
 			{
-				if(i>=j)
+				if(i>j)
 				{
 					System.out.print("*");
 				}
